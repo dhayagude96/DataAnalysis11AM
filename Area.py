5242bcca31079b9c272af659f472ea79ad395f34
dhayagude96 <dhayagude96@gmail.com>
2024-01-19 09:41:17 +0530

    Initial commit

---
+++ Area.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def RecArea(l,w):
+    return l*w
+
+def CircArea(r):
+    return 3.14*r*r
+
+def SqArea(s):
+    return s*s
+
+def triArea(b,h):
+    return 0.5*b*h
+
+def OctArea(s):
+    return 4.8*s*s
+
+def HexaArea(s):
+    return 2.59*s*s
+
